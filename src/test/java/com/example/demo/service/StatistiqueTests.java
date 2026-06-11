a5be69a90395378c9c803962bc61296d1b5f1364
Amir Khaled <tabellout.amir@gmail.com>
2026-06-12 00:53:58 +0200

    TP2 - mock de ajouter et prixMoyen avec Mockito

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
@@ -13,5 +14,22 @@ public class StatistiqueTests {
 
     @MockBean
     StatistiqueImpl statistiqueImpl;
+
+    @Test
+    void testAjouterVoiture() {
+        Voiture voiture = new Voiture("Renault", 15000);
+        statistiqueImpl.ajouter(voiture);
+        verify(statistiqueImpl, times(1)).ajouter(voiture);
+    }
+
+    @Test
+    void testPrixMoyen() {
+        Echantillon echantillon = new Echantillon(2, 17500);
+        when(statistiqueImpl.prixMoyen()).thenReturn(echantillon);
+
+        Echantillon result = statistiqueImpl.prixMoyen();
+        assertEquals(2, result.getNombreDeVoitures());
+        assertEquals(17500, result.getPrixMoyen());
+    }
 
 }
