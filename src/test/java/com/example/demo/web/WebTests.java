20f3f0332ab1297805da3af76af531482a5eb022
Amir Khaled <tabellout.amir@gmail.com>
2026-06-12 00:57:18 +0200

    TP3 - test du GET /statistique avec verification JSON

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -38,4 +38,16 @@ class WebTests {
         verify(statistiqueImpl, times(1)).ajouter(any(Voiture.class));
     }
 
+    @Test
+    void testGetStatistiques() throws Exception {
+        Echantillon echantillon = new Echantillon(2, 17500);
+        when(statistiqueImpl.prixMoyen()).thenReturn(echantillon);
+
+        mockMvc.perform(get("/statistique"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombreDeVoitures").value(2))
+                .andExpect(jsonPath("$.prixMoyen").value(17500));
+    }
+
 }
