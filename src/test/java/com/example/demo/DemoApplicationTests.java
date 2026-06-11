f172fab52ce31e82a179fbc0fd2a84ba3f64ba06
Amir Khaled <tabellout.amir@gmail.com>
2026-06-12 01:00:51 +0200

    TP4 - test de DemoApplication (contextLoads et main) pour 100% de couverture

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void main() {
+        DemoApplication.main(new String[]{"--server.port=0"});
+    }
+
+}
