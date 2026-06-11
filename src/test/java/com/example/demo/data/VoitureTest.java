67a942b7aeefeb9f4113b1fb144923746feed625
Amir Khaled <tabellout.amir@gmail.com>
2026-06-12 00:51:17 +0200

    TP1 - tests des constructeurs de Voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -2,16 +2,25 @@ package com.example.demo.data;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class VoitureTest {
 
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void creerVoitureAvecConstructeurParDefaut() {
+        Voiture voiture = new Voiture();
+        assertNull(voiture.getMarque());
+        assertEquals(0, voiture.getPrix());
+        assertEquals(0, voiture.getId());
+    }
+
+    @Test
+    void creerVoitureAvecConstructeur() {
+        Voiture voiture = new Voiture("Renault", 15000);
+        assertEquals("Renault", voiture.getMarque());
+        assertEquals(15000, voiture.getPrix());
     }
 
 }
